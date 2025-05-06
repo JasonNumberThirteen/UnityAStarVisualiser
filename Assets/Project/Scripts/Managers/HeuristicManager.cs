028b7b5742f256e5a88bdf69f9da35cb1a944de8
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-05-06 08:19:32 +0200

    Add a dropdown UI for setting heuristic type to the algorithm

--- Assets/Project/Scripts/Managers/HeuristicManager.cs
+++ Assets/Project/Scripts/Managers/HeuristicManager.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class HeuristicManager : MonoBehaviour
 {
-	private readonly HeuristicType heuristicType = HeuristicType.ManhattanDistance;
-
+	private HeuristicType heuristicType;
 	private float heuristicWeight;
+
+	public void SetHeuristicType(HeuristicType heuristicType)
+	{
+		this.heuristicType = heuristicType;
+	}
 
 	public void SetHeuristicWeight(float weight)
 	{
@@ -16,6 +20,8 @@ public class HeuristicManager : MonoBehaviour
 		var heuristicValue = heuristicType switch
 		{
 			HeuristicType.ManhattanDistance => DistanceMethods.GetOneDimensionalDistance(positionA.x, positionB.x) + DistanceMethods.GetOneDimensionalDistance(positionA.y, positionB.y),
+			HeuristicType.EuclideanDistance => Vector2.Distance(positionA, positionB),
+			HeuristicType.ChebyshevDistance => Mathf.Max(DistanceMethods.GetOneDimensionalDistance(positionA.x, positionB.x), DistanceMethods.GetOneDimensionalDistance(positionA.y, positionB.y)),
 			_ => 0f
 		};
 
