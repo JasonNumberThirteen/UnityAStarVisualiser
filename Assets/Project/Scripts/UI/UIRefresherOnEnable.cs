65c0557e381cee5b14c1aadde4fdf3f5fa461ae9
Jason <jasonxiii97@gmail.com>
2025-09-07 02:15:31 +0200

    Fix code and placement of the UI refresher on enable script

--- Assets/Project/Scripts/UI/UIRefresherOnEnable.cs
+++ Assets/Project/Scripts/UI/UIRefresherOnEnable.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(RectTransform))]
 public class UIRefresherOnEnable : MonoBehaviour
 {
-	private RectTransform rectTransform;
-
-	private void Awake()
-	{
-		rectTransform = GetComponent<RectTransform>();
-	}
-
 	private void OnEnable()
 	{
 		Canvas.ForceUpdateCanvases();
-		LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
 	}
 }
