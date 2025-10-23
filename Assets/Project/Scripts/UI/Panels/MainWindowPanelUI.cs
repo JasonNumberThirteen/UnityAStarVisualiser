3e63f0c9a2338c08ab7e8697b41b0e875c51b62d
JasonNumberThirteen <jason@jasonxiii.pl>
2025-10-23 20:56:30 +0200

    Fix glitches in the main window UI panel after loading the visualiser scene from the built application

--- Assets/Project/Scripts/UI/Panels/MainWindowPanelUI.cs
+++ Assets/Project/Scripts/UI/Panels/MainWindowPanelUI.cs
@@ -1,7 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
 public class MainWindowPanelUI : PanelUI, IPrimaryWindowElement
 {
+	private RectTransform rectTransform;
+	private CanvasUIRefresher canvasUIRefresher;
+	
 	public void SetPrimaryWindowElementActive(bool active)
 	{
 		SetActive(active);
+	}
+
+	private void Awake()
+	{
+		rectTransform = GetComponent<RectTransform>();
+		canvasUIRefresher = ObjectMethods.FindComponentOfType<CanvasUIRefresher>();
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			if(canvasUIRefresher != null)
+			{
+				canvasUIRefresher.canvasWasRebuiltEvent.AddListener(OnCanvasWasRebuilt);
+			}
+		}
+		else
+		{
+			if(canvasUIRefresher != null)
+			{
+				canvasUIRefresher.canvasWasRebuiltEvent.RemoveListener(OnCanvasWasRebuilt);
+			}
+		}
+	}
+
+	private void OnCanvasWasRebuilt()
+	{
+		LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
 	}
 }
