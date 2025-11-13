70baa55dd111880ae0a574c62b8b9011e895ce37
JasonNumberThirteen <jason@jasonxiii.pl>
2025-11-13 22:01:44 +0100

    Adjust size of the UI dropdowns and font size of their options on Android platform

---
+++ Assets/Project/Scripts/Game Objects/AndroidTextUIFontSizeAdjuster.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+#if UNITY_ANDROID
+[DefaultExecutionOrder(100)]
+#endif
+public class AndroidTextUIFontSizeAdjuster : MonoBehaviour
+{
+#if UNITY_ANDROID
+	[SerializeField] private TextMeshProUGUI[] texts;
+	[SerializeField, Min(8f)] private float fontSize;
+#endif
+
+	private void Awake()
+	{
+#if UNITY_ANDROID
+		texts.ForEach(AdjustTextUIFontSizeIfNeeded);
+#else
+		Destroy(gameObject);
+#endif
+	}
+
+#if UNITY_ANDROID
+	private void AdjustTextUIFontSizeIfNeeded(TextMeshProUGUI text)
+	{
+		if(text != null)
+		{
+			text.fontSize = fontSize;
+		}
+	}
+#endif
+}
