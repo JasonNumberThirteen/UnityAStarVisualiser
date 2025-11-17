7c7401c930bccadf859bc6553975c742f4e9d326
JasonNumberThirteen <jason@jasonxiii.pl>
2025-11-17 13:15:18 +0100

    Remove compilation directives from the mouse methods script

--- Assets/Project/Scripts/Static Methods/MouseMethods.cs
+++ Assets/Project/Scripts/Static Methods/MouseMethods.cs
@@ -1,11 +1,7 @@
-#if !UNITY_ANDROID
 using UnityEngine;
 using UnityEngine.InputSystem;
-#endif
 
 public static class MouseMethods
 {
-#if !UNITY_ANDROID
 	public static Vector2 GetMousePosition() => Mouse.current.position.ReadValue();
-#endif
 }
