46e92dd3ccf7a037c3c107f15b657d836bc73608
JasonNumberThirteen <jason@jasonxiii.pl>
2025-11-23 12:09:27 +0100

    Adjust the map area indicator for taking a map screenshot

--- Assets/Project/Scripts/Game Objects/Indicators/MapAreaIndicator.cs
+++ Assets/Project/Scripts/Game Objects/Indicators/MapAreaIndicator.cs
@@ -2,7 +2,7 @@ using DG.Tweening;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
-public class MapAreaIndicator : MonoBehaviour
+public class MapAreaIndicator : MonoBehaviour, IMapScreenshotTakerElement
 {
 	[SerializeField][Range(0f, 1f)] private float minimumAlphaWhileFading;
 	[SerializeField][Min(0f)] private float fadeDuration = 5f;
@@ -10,6 +10,26 @@ public class MapAreaIndicator : MonoBehaviour
 	private SpriteRenderer spriteRenderer;
 	private MapAreaManager mapAreaManager;
 	private Tween fadeTween;
+	private float lastAlpha;
+
+	private static readonly float ALPHA_WHILE_TAKING_MAP_SCREENSHOT = 1f;
+
+	public void AdjustForTakingMapScreenshot(bool started)
+	{
+		if(started)
+		{
+			fadeTween?.Pause();
+
+			lastAlpha = spriteRenderer.color.a;
+
+			SetAlpha(ALPHA_WHILE_TAKING_MAP_SCREENSHOT);
+		}
+		else
+		{
+			SetAlpha(lastAlpha);
+			fadeTween?.Play();
+		}
+	}
 
 	public void SetActive(bool active)
 	{
@@ -63,5 +83,10 @@ public class MapAreaIndicator : MonoBehaviour
 		{
 			fadeTween = spriteRenderer.DOFade(minimumAlphaWhileFading, fadeDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
 		}
+	}
+
+	private void SetAlpha(float alpha)
+	{
+		spriteRenderer.color = ColorMethods.GetColorWithSetAlpha(spriteRenderer.color, alpha);
 	}
 }
--- Assets/Project/Scripts/Game Objects/Managers/MapScreenshotTakerElementsManager.cs
+++ Assets/Project/Scripts/Game Objects/Managers/MapScreenshotTakerElementsManager.cs
@@ -14,6 +14,7 @@ public class MapScreenshotTakerElementsManager : MonoBehaviour
 		mapScreenshotTaker = ObjectMethods.FindComponentOfType<MapScreenshotTaker>();
 		mapTilesPathTrailManager = ObjectMethods.FindComponentOfType<MapTilesPathTrailManager>();
 
+		mapScreenshotTakerElements.AddRange(ObjectMethods.FindInterfacesOfType<IMapScreenshotTakerElement>());
 		RegisterToListeners(true);
 	}
 
